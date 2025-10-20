5ea64cbb30ca2b6591886df7bb7a9eec245e9d3c
Agustin Benedetti <agustin.benedetti.work@gmail.com>
2025-10-20 10:22:21 -0300

    edit front

--- src/serivce/authService.js
+++ src/serivce/authService.js
@@ -1,3 +1,5 @@
+import ENVIRONMENT from "../config/environment"
+
 export async function register (username, email, password){
     try{
         const body = {
@@ -9,7 +11,7 @@ export async function register (username, email, password){
     
         //Fetch es una funcion nativa de JS para hacer consultas HTTP
         const response_http = await fetch(
-            'http://localhost:8080/api/auth/register',
+        `${ENVIRONMENT.API_URL}/api/auth/register`,
             {
                 method: 'POST',
                 headers: {
@@ -39,7 +41,7 @@ export async function login (email, password){
         } 
     
         const response_http = await fetch(
-                'http://localhost:8080/api/auth/login',
+                `${ENVIRONMENT.API_URL}/api/auth/login`,
             {
                 method: 'POST',
                 headers: {
